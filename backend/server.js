4dc429abacbca5c15ad80f73cd91e08beb862b1a
Zeplin Rowe <rowe_michael101@yahoo.com>
2025-09-17 16:55:15 -0400

    completed backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
-import connectDB from "./config/db.js";
-
 const app = express();
 
-app.get("/products", (req, res) => {});
+app.use(express.json()); // allows us to accept JSON data in the req.body
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/products", productRoutes);
 
 app.listen(5001, () => {
   connectDB();
