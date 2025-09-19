486b6695bdb6baa35dcffc779c23a55fef6257da
Zeplin Rowe <rowe_michael101@yahoo.com>
2025-09-19 12:29:24 -0400

    project completed

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+
 import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/product.route.js";
@@ -9,10 +11,20 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // allows us to accept JSON data in the req.body
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
